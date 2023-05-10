a87d7756a332737f1362d5da342be4d14ec75317
Vidhi-s <vidhishah4480@gmail.com>
2023-05-10 20:16:17 +0530

    finished project

--- lib/story.dart
+++ lib/story.dart
@@ -1,12 +1,9 @@
-class story{
-  String storyTitle='';
-  String choice1='';
-  String choice2='';
-  story({ required this.storyTitle,required this.choice1,required this.choice2}){
-
-  }
-
-
-
-
+class story {
+  String storyTitle = '';
+  String choice1 = '';
+  String choice2 = '';
+  story(
+      {required this.storyTitle,
+      required this.choice1,
+      required this.choice2}) {}
 }
